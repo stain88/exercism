c54da77b78a6a7a46acbf5d32903f8a5d431af67
stain88 <marcbaghdadi@gmail.com>
2015-11-29 19:36:06 +0000

    bob javascript

--- javascript/bob/bob.js
+++ javascript/bob/bob.js
@@ -9,7 +9,6 @@ Bob.prototype.hey = function(input) {
 //
 // YOUR CODE GOES HERE
 //
-  console.log("\""+input+"\"");
   if (input.charAt(input.length-1)==='?') {
     if (/^[A-Z\s]+$/.test(input.substring(0,input.length-1))) return "Whoa, chill out!";
     return "Sure.";
@@ -17,6 +16,7 @@ Bob.prototype.hey = function(input) {
   if (/^[\d,\s]+$/.test(input)) return "Whatever.";
   if (/^[A-Z\s,\d\%\^\*\@\#\$\(!\u00c4\u00dc]+!?$/.test(input)) return "Whoa, chill out!";
   if (/^\s*$/.test(input)) return "Fine. Be that way!";
+  
   return "Whatever.";
 };
 
