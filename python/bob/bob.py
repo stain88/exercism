8ff3822888ed2ba190ce704f1e2f5ab14ff22444
stain88 <marcbaghdadi@gmail.com>
2017-02-16 21:07:05 +0000

    python bob submit

--- python/bob/bob.py
+++ python/bob/bob.py
@@ -1,8 +1,10 @@
-#
-# Skeleton file for the Python "Bob" exercise.
-#
-
+import re
 
 def hey(what):
-
-    return
+	if re.match(r'^\s+$', what) or what == "":
+		return 'Fine. Be that way!'
+	if what == what.upper() and not re.match(r'^[\d, \?]+$', what):
+		return 'Whoa, chill out!'
+	if what.strip()[-1:] == '?':
+		return 'Sure.'
+	return 'Whatever.'
