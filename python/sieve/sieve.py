fc778a0d299439002629b8b18481d4a928eeef79
stain88 <marcbaghdadi@gmail.com>
2017-02-19 20:54:20 +0000

    python submit sieve v2

--- python/sieve/sieve.py
+++ python/sieve/sieve.py
@@ -1,5 +1,13 @@
 def sieve(input):
-	primes = range(2,input+1)
-	for p in primes:
-		primes = filter(lambda x: x==p or x % p != 0, primes)
-	return primes
+	primes = [None, None] + [True] * (input - 1)
+	for p in range(2, input+1):
+		if primes[p]:
+			for m in range(2*p,input+1,p):
+				primes[m] = None
+	return [i for i, p in enumerate(primes) if p]
+
+# def sieve(input):
+# 	primes = range(2,input+1)
+# 	for p in primes:
+# 		primes = filter(lambda x: x==p or x % p != 0, primes)
+# 	return primes
