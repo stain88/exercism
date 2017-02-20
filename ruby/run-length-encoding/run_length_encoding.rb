5201a3605ef631af3676e1e747ed88af05c07a7f
stain88 <marcbaghdadi@gmail.com>
2017-02-20 22:15:49 +0000

    ruby submit run-length-encoding v2

--- ruby/run-length-encoding/run_length_encoding.rb
+++ ruby/run-length-encoding/run_length_encoding.rb
@@ -1,17 +1,17 @@
 class RunLengthEncoding
-	def self.encode input
-		input.scan(/(.)(\1*)/).map { |m|
-			m[1].length == 0 ? m[0] : "#{m[1].length+1}#{m[0]}"
-		}.join
-	end
+  def self.encode input
+    input.scan(/(.)(\1*)/).map { |m|
+      m[1].length == 0 ? m[0] : "#{m[1].length+1}#{m[0]}"
+    }.join
+  end
 
-	def self.decode input
-		input.scan(/((\d*)(.))/).map { |m|
-			m[1].empty? ? m[2] : "#{m[2]*m[1].to_i}"
-		}.join
-	end
+  def self.decode input
+    input.scan(/((\d*)(.))/).map { |m|
+      m[1].empty? ? m[2] : "#{m[2]*m[1].to_i}"
+    }.join
+  end
 end
 
 module BookKeeping
-	VERSION = 2
+  VERSION = 2
 end
