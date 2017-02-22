62d573dfca24a8b10612fb265aad06b4089ebaf1
Marc Baghdadi <yourname@apple.com>
2017-02-22 16:29:47 +0000

    ruby submit accumulate

---
+++ ruby/accumulate/accumulate.rb
@@ -0,0 +1,11 @@
+module Accumulate
+	def accumulate
+		self.map { |item| Proc.new.call item }
+	end
+end
+
+module BookKeeping
+	VERSION = 1
+end
+
+Array.include Accumulate
--- ruby/accumulate/accumulate_test.rb
+++ ruby/accumulate/accumulate_test.rb
@@ -9,7 +9,6 @@ class ArrayTest < Minitest::Test
   end
 
   def test_accumulate_squares
-    skip
     result = [1, 2, 3].accumulate do |number|
       number * number
     end
@@ -17,19 +16,16 @@ class ArrayTest < Minitest::Test
   end
 
   def test_accumulate_upcases
-    skip
     result = %w(hello world).accumulate(&:upcase)
     assert_equal %w(HELLO WORLD), result
   end
 
   def test_accumulate_reversed_strings
-    skip
     result = %w(the quick brown fox etc).accumulate(&:reverse)
     assert_equal %w(eht kciuq nworb xof cte), result
   end
 
   def test_accumulate_recursively
-    skip
     result = %w(a b c).accumulate do |char|
       %w(1 2 3).accumulate do |digit|
         "#{char}#{digit}"
@@ -39,7 +35,6 @@ class ArrayTest < Minitest::Test
   end
 
   def test_do_not_change_in_place
-    skip
     original = [1, 2, 3]
     copy = original.dup
     original.accumulate { |n| n * n }
@@ -63,7 +58,6 @@ class ArrayTest < Minitest::Test
   # http://ruby-doc.org/docs/ruby-doc-bundle/UsersGuide/rg/constants.html
 
   def test_bookkeeping
-    skip
     assert_equal 1, BookKeeping::VERSION
   end
 end
